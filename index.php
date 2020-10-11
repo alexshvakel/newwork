bd7183b48c00baadb49fe69713d6b9f90315b1e1
Alex <shvakel777@gmail.com>
2020-10-11 19:02:27 +0300

    l05: Added arrays

--- index.php
+++ index.php
@@ -2,3 +2,53 @@
 
 echo "Hello world!" . PHP_EOL;
 
+$menu = [
+            [
+            'title' => 'Samples',
+            'children' =>
+                [
+                    [
+                            'url'=> '/',
+                            'title' => 'Home',
+
+                    ],
+                    [
+                    'url'=> '/samples/arrays.php',
+                    'title' => 'Arrays',
+
+                    ],
+                    [
+                    'url' => '/samples/basics.php',
+                    'title' => 'Basics',
+                    ],
+                    [
+                    'url'=> '/samples/branching.php',
+                    'title' => 'Branching'
+                    ],
+                    [
+                    'url' => '/samples/home.php',
+                    'title' => 'Home',
+                    ],
+                    [
+                    'url' => '/samples/html.php',
+                    'title' => 'Html',
+                    ],
+                    [
+                    'url' => '/samples/math.php',
+                    'title' => 'Math',
+                    ],
+                    [
+                    'url' => '/samples/types.php',
+                    'title' => 'Types',
+                    ]
+
+                ],
+            ],
+        ];
+var_dump($menu);
+
+
+
+
+
+
