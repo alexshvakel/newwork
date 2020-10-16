6691df020b9c92e50f25b0630da0f17a90ac2f6d
Alex <shvakel777@gmail.com>
2020-10-16 11:34:43 +0300

    l05: Added homearray

--- samples/homearray.php
+++ samples/homearray.php
@@ -1,32 +1,43 @@
 <?php
 
-$task1 = [
-    'id1' => 777,
-    'title' => 'qwerty',
-    'description' => 'project preparation',
-    'owner' => 'Alex',
-    'deadline' => '26.01.21',
-    'status' => 'in process'
+$task_manager =
+    [
+    'task1' =>
+        [
+        'id1' => 777,
+        'title' => 'qwerty',
+        'description' => 'project preparation',
+        'owner' => 'Alex',
+        'deadline' => '26.01.21',
+        'status' => 'in process'
+        ],
+        [
+            'task2' =>
+                [
+                'id1' => 333,
+                'title' => 'Hello world',
+                'description' => 'development',
+                'owner' => 'Shvakel',
+                'deadline' => '27.01.21',
+                'status' => 'in process'
 
-];
-$task2 = [
-    'id1' => 333,
-    'title' => 'Hello world',
-    'description' => 'development',
-    'owner' => 'Shvakel',
-    'deadline' => '27.01.21',
-    'status' => 'in process'
+                ]
+        ],
+        [
+            'task3' =>
+                [
+                'id1' => 111,
+                'title' => 'Success',
+                'description' => 'Reach the goal',
+                'owner' => 'Evgenevich',
+                'deadline' => '28.01.21',
+                'status' => 'in process'
 
-];
-$task3 = [
-    'id1' => 111,
-    'title' => 'Success',
-    'description' => 'Reach the goal',
-    'owner' => 'Evgenevich',
-    'deadline' => '28.01.21',
-    'status' => 'in process'
-];
-var_dump($task1, $task2, $task3);
+                ]
+        ]
+    ];
+
+var_dump($task_manager);
 
 
 
