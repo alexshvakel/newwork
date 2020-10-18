abba5900ef9bb9195e1ab6deb911a28bede87ba6
Alex <shvakel777@gmail.com>
2020-10-18 17:21:19 +0300

    l06: Added loops demonstration

--- index.php
+++ index.php
@@ -1,53 +1,42 @@
 <?php
 
-echo "Hello world!" . PHP_EOL;
+$files = scandir(__DIR__);
+$elements = array_filter($files, static function (string $element ){
+        return $element[0] !== '.' && stripos($element, 'docker') === false;
+});
+$html ='';
 
-$menu = [
-            [
-            'title' => 'Samples',
-            'children' =>
-                [
-                    [
-                            'url'=> '/',
-                            'title' => 'Home',
+foreach ($elements as $element){
+    $rout = __DIR__ . '/' . $element;
+    if (is_dir($rout)) {
+        $dir = scandir($rout);
+        $nestedElements = array_filter( $dir, static function ( string $elment){
+            return $elment [0] !== '.';
+        });
+        foreach ($nestedElements as $nestedElement) {
+            $html .= "<li><a href='/{$element}/{$nestedElement}'>{$element}/{$nestedElement}</a></li>";
+        }
+    } else {
+        $html .= "<li><a href='/{$element}'>{$element}</a></li>";
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+        <ul>
+            <?= $html ?>
+        </ul>
 
-                    ],
-                    [
-                    'url'=> '/samples/arrays.php',
-                    'title' => 'Arrays',
-
-                    ],
-                    [
-                    'url' => '/samples/basics.php',
-                    'title' => 'Basics',
-                    ],
-                    [
-                    'url'=> '/samples/branching.php',
-                    'title' => 'Branching'
-                    ],
-                    [
-                    'url' => '/samples/home.php',
-                    'title' => 'Home',
-                    ],
-                    [
-                    'url' => '/samples/html.php',
-                    'title' => 'Html',
-                    ],
-                    [
-                    'url' => '/samples/math.php',
-                    'title' => 'Math',
-                    ],
-                    [
-                    'url' => '/samples/types.php',
-                    'title' => 'Types',
-                    ]
-
-                ],
-            ],
-        ];
-var_dump($menu);
-
-
+</body>
+</html>
 
 
 
